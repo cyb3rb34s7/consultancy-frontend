5a0423168785307394b12c91686bd816881b644e
Raja Kumar Singh <raja@gmail.com>
2024-06-18 22:44:11 +0530

    new change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["@chakra-ui/react",'axios'],
+      external: ["@chakra-ui/react"],
     },
   },
 });
